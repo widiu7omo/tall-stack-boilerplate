7cf2bc6098921a5072abb69589d83be5ba50d500
Danar Widi <widiu7omo@gmail.com>
2023-04-06 09:16:20 +0800

    Interact with media

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,18 +4,24 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Cog\Laravel\Ban\Traits\Bannable;
-use \Cog\Contracts\Ban\Bannable as BannableContract;
+use Cog\Contracts\Ban\Bannable as BannableContract;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements BannableContract
+class User extends Authenticatable implements BannableContract, FilamentUser, HasMedia
 {
     use HasApiTokens, HasFactory, Notifiable;
     use HasRoles;
     use Bannable;
+    use InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
@@ -46,4 +52,17 @@ class User extends Authenticatable implements BannableContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        return true;
+    }
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Manipulations::FIT_CROP, 300, 300)
+            ->nonQueued();
+    }
 }

5a9440c94566f39efff523c5b04ea7b7c9ebf7bf
Danar Widi <widiu7omo@gmail.com>
2023-04-06 10:29:03 +0800

    Shield page backup and health

---
+++ src/app/Filament/Pages/Backup.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+use ShuvroRoy\FilamentSpatieLaravelBackup\Pages\Backups as BaseBackup;
+
+class Backup extends BaseBackup
+{
+    use HasPageShield;
+}
---
+++ src/app/Filament/Pages/Health.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+use ShuvroRoy\FilamentSpatieLaravelHealth\Pages\HealthCheckResults;
+
+class Health extends HealthCheckResults
+{
+    use HasPageShield;
+}

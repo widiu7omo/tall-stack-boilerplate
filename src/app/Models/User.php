4999c070072459ae38a85da6af0e74fcce27f776
Danar Widi <widiu7omo@gmail.com>
2023-04-08 00:10:53 +0800

    add taggable for user

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -15,6 +15,7 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Permission\Traits\HasRoles;
+use Spatie\Tags\HasTags;
 
 class User extends Authenticatable implements BannableContract, FilamentUser, HasMedia
 {
@@ -22,7 +23,7 @@ class User extends Authenticatable implements BannableContract, FilamentUser, Ha
     use HasRoles;
     use Bannable;
     use InteractsWithMedia;
-
+    use HasTags;
     /**
      * The attributes that are mass assignable.
      *
